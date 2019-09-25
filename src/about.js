cd24038db8a0f32b164eb4c8739095ac58adf6c9
Varun Kumar <varunkumaranalysis@gmail.com>
2019-09-25 09:27:39 -0400

    Done.

---
+++ src/about.js
@@ -0,0 +1,13 @@
+import React, { Fragment, Component } from "react";
+
+class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is my about component!</h1>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ src/home.js
@@ -0,0 +1,13 @@
+import React, { Fragment, Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Home!</h1>
+      </div>
+    );
+  }
+}
+
+export default Home;
